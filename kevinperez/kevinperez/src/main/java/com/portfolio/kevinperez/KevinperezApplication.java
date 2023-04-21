186b60e88d1d96305fba02832c34beb98f7c112f
kevinperez94 <kevinperez94@outlook.com.ar>
2023-04-21 16:32:17 -0300

    ok2

---
+++ kevinperez/kevinperez/src/main/java/com/portfolio/kevinperez/KevinperezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.kevinperez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinperezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinperezApplication.class, args);
+	}
+
+}
---
+++ kevinperez/kevinperez/src/test/java/com/portfolio/kevinperez/KevinperezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.kevinperez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinperezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
